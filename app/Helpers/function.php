a15e487d2bf4abe76fe916dde2b2a2bd84bab8ba
pttuoi99 <tuoipt.99@gmail.com>
2020-07-01 16:53:34 +0700

    abc

--- app/Helpers/function.php
+++ app/Helpers/function.php
@@ -1,53 +1,45 @@
 <?php
-if (!function_exists('upload_image'))// trả về giá trị TRUE nếu hàm đã tồn tại và ngược lại FALSE nếu chưa tồn tại. -> neu ham ko ton tai moi thuc hien upload_image
-{
-
+if (!function_exists('upload_image')) {
+    /**
+     * @param $file [tên file trùng tên input]
+     * @param array $extend [ định dạng file có thể upload được]
+     * @return array|int [ tham số trả về là 1 mảng - nếu lỗi trả về int ]
+     */
     function upload_image($file, $folder = '', array $extend = array())
     {
         $code = 1;
-        // lay duong dan anh
+// lay duong dan anh
         $baseFilename = public_path() . '/uploads/' . $_FILES[$file]['name'];
-
-        // thong tin file
+// thong tin file
         $info = new SplFileInfo($baseFilename);
-        // SplFileInfo la class cap cap thong tin cua file nhu ten, duoi mo rong ...
-
-        // duoi file
+// duoi file
         $ext = strtolower($info->getExtension());
-
-        // kiem tra dinh dang file
+// kiem tra dinh dang file
         if (!$extend) {
-            $extend = ['png', 'jpg', 'jpeg'];
+            $extend = ['png', 'jpg', 'jpeg','gif'];
         }
-
         if (!in_array($ext, $extend)) {
             return $data['code'] = 0;
         }
-
-        // Tên file mới
+// Tên file mới
         $nameFile = trim(str_replace('.' . $ext, '', strtolower($info->getFilename())));
-        $filename = date('Y-m-d__') . str_slug($nameFile) . '.' . $ext;
-
-        // thu muc goc de upload
+        $filename = date('Y-m-d__') .$nameFile . '.' . $ext;
+// thu muc goc de upload
         $path = public_path() . '/uploads/' . date('Y/m/d/');
-
         if ($folder) {
             $path = public_path() . '/uploads/' . $folder . '/' . date('Y/m/d/');
         }
-
         if (!\File::exists($path)) {
             mkdir($path, 0777, true);
         }
-
+// di chuyen file vao thu muc uploads
         move_uploaded_file($_FILES[$file]['tmp_name'], $path . $filename);
-
         $data = [
-            'name'     => $filename,
-            'code'     => $code,
-            'path_img' => 'uploads/' . $filename,
+            'name' => $filename,
+            'code' => $code,
+            'path_img' => 'uploads/' . $filename
         ];
         return $data;
-
     }
 }
 if (!function_exists('pare_url_file')) {
@@ -56,7 +48,6 @@ if (!function_exists('pare_url_file')) {
         if (!$image) {
             return '/images/no-image.jpg';
         }
-
         $explode = explode('__', $image);
         if (isset($explode[0])) {
             $time = str_replace('_', '/', $explode[0]);
@@ -64,12 +55,3 @@ if (!function_exists('pare_url_file')) {
         }
     }
 }
-
-if (!function_exists('str_slug')) {
-
-    function str_slug($title, $separator = '-')
-    {
-        return Str::slug($title, $separator);
-    }
-}
-?>
